0d596150881be8d8528e56b1e075bee6e7fd7d16
jingjing.xiong <hazelxiong21@gmail.com>
2024-05-13 17:03:33 +0200

    Build app successfully(./dist), use capacitor to build mobile app running on ios.

---
+++ capacitor.config.ts
@@ -0,0 +1,14 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'take_home_assignment_neolocus',
+  webDir: 'dist',
+  // bundledWebRuntime: false,
+  // server: {
+  //   url: 'http://localhost:5173/',
+  //   cleartext: true, 
+  // },
+};
+
+export default config;
